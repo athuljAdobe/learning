6db5283e84c21f891487d5d2c914727a80192079
Athul Ganesh <athulgnsh55@gmail.com>
2026-03-10 08:24:22 +0530

    sign in bar done

---
+++ blocks/SignIn/SignIn.js
@@ -0,0 +1,17 @@
+export default function decorate(block) {
+
+  const signInText = block.querySelector('p');
+
+  if (signInText) {
+
+    // make it look clickable
+    signInText.style.cursor = "pointer";
+
+    // redirect on click
+    signInText.addEventListener("click", () => {
+      window.location.href = "/login";
+    });
+
+  }
+
+}
